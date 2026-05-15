c8c472b690c4357d020fa02e5b2c1be51735357b
JiWoo Kim <wldnkim00@gmail.com>
2026-05-15 16:49:11 +0900

    [refactor] PlayerSkillData 제거
    PlayerSkillData 제거
    
    PlayerSkillData.cs 삭제 (무기 시스템으로 대체)
    PlayerSkillData ScriptableObject 에셋 삭제

--- Turn_Based_Game_3D/Assets/Scripts/Scriptable/PlayerSkillData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "PlayerSkillData", menuName = "Scriptable Objects/PlayerSkillData")]
-public class PlayerSkillData : SkillData
-{
-    [Header("Skill Stats")]
-    public uint Power;
-    public uint Cost;
-}
